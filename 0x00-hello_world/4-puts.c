c236dd4e92b053d2eec52b60dd6ac722e21587fb
55her <mekedesabera55@gmail.com>
2022-04-08 08:28:14 -0700

    skrskr

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing the function 
+ */
+int main(void)
+[
+puts("\"programing is like building a multilungal puzzle");
+return (0);
+]
+
